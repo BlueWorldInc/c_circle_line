efeff5aa4daccf9b51b7d5ab2e3dada93919e9ec
BlueWorldInc <keamybakunin@gmail.com>
2020-07-05 12:20:46 +0200

    [add] added dda algorithm

---
+++ include/drawAlgos.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+#define BASELINE SCREEN_HEIGHT * 0.75
+
+void drawMainWindow(SDL_Renderer* renderer);
